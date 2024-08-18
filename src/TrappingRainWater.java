80ecd0f98db047fbfdaa8fe2f16b5347199da6cf
Li Xie <xlabcba@gmail.com>
2024-08-17 17:34:44 -0700

    Updated 42.TrappingRainWater

--- src/TrappingRainWater.java
+++ src/TrappingRainWater.java
@@ -1,6 +1,9 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * 42. Trapping Rain Water (https://leetcode.com/problems/trapping-rain-water/description/)
+ */
 public class TrappingRainWater {
 
     /****************** Solution 1: Descending Monotonic Stack **********************/
@@ -28,9 +31,9 @@ public class TrappingRainWater {
 
     /************************ Solution 2: Two Pointers **************************/
     /**
-     * lHighest 标记左墙高度，rHighest 标记右墙高度
+     * lm 标记左墙最高高度，rm 标记右墙最高高度
      * l, r 分别标记当前位置
-     * 每次取左右墙中较矮的一个，这样保证两侧不断向中间最高墙推进，左右墙即为短板，决定了水位
+     * 每次取左右位置中较矮的一个，这样保证两侧不断向中间最高墙推进，左右墙即为短板，决定了水位
      * 如果当前高度比墙矮，则高度差为这一位置的水位
      * 如果当前高度比墙高，则刷新墙高
      *
@@ -40,20 +43,20 @@ public class TrappingRainWater {
      */
     public int trap(int[] height) {
         int res = 0;
-        int l = 0, r = height.length - 1, lHighest = 0, rHighest = 0;
-        while (l <= r) { // 注意！！！要有等号！！！最后汇合处可能也有积水！！！
-            if (lHighest < rHighest) {
-                if (height[l] < lHighest) {
-                    res += lHighest - height[l];
-                } else if (height[l] > lHighest) {
-                    lHighest = height[l];
+        int l = 0, r = height.length - 1, lm = 0, rm = height.length - 1;
+        while (l < r) {
+            if (height[l] <= height[r]) {
+                if (height[l] < height[lm]) {
+                    res += height[lm] - height[l];
+                } else {
+                    lm = l;
                 }
                 l++;
             } else {
-                if (height[r] < rHighest) {
-                    res += rHighest - height[r];
-                } else if (height[r] > rHighest) {
-                    rHighest = height[r];
+                if (height[r] < height[rm]) {
+                    res += height[rm] - height[r];
+                } else {
+                    rm = r;
                 }
                 r--;
             }
@@ -63,8 +66,8 @@ public class TrappingRainWater {
 
     public static void main(String[] args) {
         TrappingRainWater solution = new TrappingRainWater();
-        System.out.println(solution.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
-        System.out.println(solution.trap(new int[]{4,2,0,3,2,5}));
+        System.out.println(solution.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1})); // 6
+        System.out.println(solution.trap(new int[]{4,2,0,3,2,5})); // 9
     }
 
 }
