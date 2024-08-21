4afa3a3ab37229d5c81412faac4cb14ca47d83af
Li Xie <xlabcba@gmail.com>
2024-08-20 18:41:19 -0700

    LC402

--- src/RemoveKDigits.java
+++ src/RemoveKDigits.java
@@ -1,10 +1,16 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * 402. Remove K Digits (https://leetcode.com/problems/remove-k-digits/description/)
+ */
 public class RemoveKDigits {
 
     /************* Solution 1: Greedy + Monotonic Stack ***************/
     /**
+     * 维护单调递增stack，相等可以不移除，如：【1，2，2，3】
+     * 难点在于greedy即最优解，不用每遍移除一个数字，而是只一遍，一旦有递减，就可以把前面大的移除！
+     *
      * Time: O(N) each char at most enter & exit stack once
      * Space: O(N) by stack
      */
@@ -16,7 +22,7 @@ public class RemoveKDigits {
                 stack.pop();
                 k--;
             }
-            // if用于去除leading zeros
+            // if用于去除leading zeros, 也可以StringBuilder的时候去除
             if (!stack.isEmpty() || c != '0') stack.push(c);
         }
         // 如果是单调递增的字符串，最后可能有剩余的移除名额
@@ -28,9 +34,9 @@ public class RemoveKDigits {
 
     public static void main(String[] args) {
         RemoveKDigits solution = new RemoveKDigits();
-        System.out.println(solution.removeKdigits("1234567890", 9));
-        System.out.println(solution.removeKdigits("1432219", 3));
-        System.out.println(solution.removeKdigits("10200", 1));
-        System.out.println(solution.removeKdigits("10", 2));
+        System.out.println(solution.removeKdigits("1234567890", 9)); // 0
+        System.out.println(solution.removeKdigits("1432219", 3)); // 1219
+        System.out.println(solution.removeKdigits("10200", 1)); // 200
+        System.out.println(solution.removeKdigits("10", 2)); // 0
     }
 }
