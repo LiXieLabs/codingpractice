843007883f3cd1245d592c55b8c342b9d856147c
Li Xie <xlabcba@gmail.com>
2024-09-23 21:35:34 -0700

    LC217&243

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,9 +9,6 @@ public class ContainsDuplicate {
 
     /*********** Solution 1: HashSet ****************/
     /**
-     * Sort 时间换空间
-     * Time: O(NlogN)   Space: O(1)
-     *
      * HashSet 空间换时间
      * Time: O(N)       Space: O(N)
      */
@@ -18,6 +16,19 @@ public class ContainsDuplicate {
         Set<Integer> set = new HashSet<>();
         for (int n : nums) {
             if (!set.add(n)) return true;
+        }
+        return false;
+    }
+
+    /*********** Solution 2: Arrays.sort in place ****************/
+    /**
+     * Sort 时间换空间
+     * Time: O(NlogN)   Space: O(1)
+     */
+    public boolean containsDuplicate1(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) return true;
         }
         return false;
     }
--- src/ShortestWordDistance.java
+++ src/ShortestWordDistance.java
@@ -1,3 +1,6 @@
+/**
+ * 243. Shortest Word Distance (https://leetcode.com/problems/shortest-word-distance/description/)
+ */
 public class ShortestWordDistance {
 
     /********** Solution 1: One Pass ***************/
@@ -6,14 +9,14 @@ public class ShortestWordDistance {
      * Space: O(1)
      */
     public int shortestDistance(String[] wordsDict, String word1, String word2) {
-        int p1 = -1, p2 = -1, res = wordsDict.length;
+        int p1 = -wordsDict.length, p2 = -wordsDict.length, res = wordsDict.length;
         for (int i = 0; i < wordsDict.length; i++) {
             if (word1.equals(wordsDict[i])) {
-                if (p2 != -1) res = Math.min(res, i - p2);
+                res = Math.min(res, i - p2);
                 p1 = i;
             }
             if (word2.equals(wordsDict[i])) {
-                if (p1 != -1) res = Math.min(res, i - p1);
+                res = Math.min(res, i - p1);
                 p2 = i;
             }
         }
