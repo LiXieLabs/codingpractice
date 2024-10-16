3ecc1ce84ed57c068da1ca5929b5e30d8309dbe5
Li Xie <xlabcba@gmail.com>
2024-10-15 21:41:59 -0700

    LC45&55

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -1,3 +1,6 @@
+/**
+ * 55. Jump Game (https://leetcode.com/problems/jump-game/description/)
+ */
 public class JumpGame {
 
     /********** Solution 1: 1D Greedy Algo ***************/
@@ -5,20 +8,21 @@ public class JumpGame {
      * Time: O(N)   Space: O(1)
      */
     public boolean canJump(int[] nums) {
-        int curCanReach = 0, nexCanReach = 0;
-        for (int i = 0; curCanReach < nums.length - 1; curCanReach = nexCanReach) {
-            while (i <= curCanReach) {
-                nexCanReach = Math.max(nexCanReach, i + nums[i++]);
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (maxReach < i) {
+                return false;
             }
-            if (nexCanReach == curCanReach) return false;
+            int curReach = i + nums[i];
+            maxReach = Math.max(maxReach, curReach);
         }
         return true;
     }
 
     public static void main(String[] args) {
         JumpGame solution = new JumpGame();
-        System.out.println(solution.canJump(new int[]{2,3,1,1,4}));
-        System.out.println(solution.canJump(new int[]{3,2,1,0,4}));
+        System.out.println(solution.canJump(new int[]{2,3,1,1,4})); // true
+        System.out.println(solution.canJump(new int[]{3,2,1,0,4})); // false
     }
 
 }
--- src/JumpGameII.java
+++ src/JumpGameII.java
@@ -1,9 +1,12 @@
+/**
+ * 45. Jump Game II (https://leetcode.com/problems/jump-game-ii/description/)
+ */
 public class JumpGameII {
 
     /*********** Solution 1: 1D Greedy Algo **************/
     /**
      * 与 55. Jump Game (https://leetcode.com/problems/jump-game/)
-     * 区别是需要count记录步数
+     * 区别是需要steps记录步数
      *
      * 类似
      *   1024. Video Stitching (https://leetcode.com/problems/video-stitching/)：
@@ -12,19 +15,23 @@ public class JumpGameII {
      * Time: O(N)   Space: O(1)
      */
     public int jump(int[] nums) {
-        int count = 0, curCanReach = 0, nexCanReach = 0;
-        for (int i = 0; curCanReach < nums.length - 1; curCanReach = nexCanReach, count++) {
-            while (i <= curCanReach) {
-                nexCanReach = Math.max(nexCanReach, i + nums[i++]);
+        int steps = 0, maxReach = 0, i = 0;
+        while (maxReach < nums.length - 1) {
+            int nexReach = maxReach;
+            while (i <= maxReach) {
+                nexReach = Math.max(nexReach, i + nums[i++]);
             }
-            if (nexCanReach == curCanReach) return -1; // Optional if it is guaranteed to reach the end.
+            if (nexReach == maxReach) return -1; // Optional if it is guaranteed to reach the end.
+            maxReach = nexReach;
+            steps++;
         }
-        return count;
+        return steps;
     }
 
     public static void main(String[] args) {
         JumpGameII solution = new JumpGameII();
         System.out.println(solution.jump(new int[]{2,3,1,1,4})); // 2
         System.out.println(solution.jump(new int[]{2,3,0,1,4})); // 2
+        System.out.println(solution.jump(new int[]{2,2,0,0,4})); // -1
     }
 }
