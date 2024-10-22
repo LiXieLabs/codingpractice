7b1f19026a1d51aeba90ca06ca13de0f49610a15
Li Xie <xlabcba@gmail.com>
2024-10-21 20:41:29 -0700

    LC143

--- src/ReorderList.java
+++ src/ReorderList.java
@@ -2,13 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * 143. Reorder List (https://leetcode.com/problems/reorder-list/description/)
+ */
 public class ReorderList {
 
-    /************** Soluion 1: 3 Passes (找中点, reverse list, merge 2 lists **************/
+    /************** Soluion 1: 3 Passes (找中点, reverse list, merge 2 lists) **************/
     /**
      * Time: O(3N) = O(N)  Space: O(1)
      */
-    public void reorderList(ListNode head) {
+    public void reorderList1(ListNode head) {
         // Step 1
         // 找到中点
         // 1 -> 2 -> 3 -> 4 is 3
@@ -53,6 +56,49 @@ public class ReorderList {
         }
     }
 
+    /************** Soluion 2: 另一种 3 Passes (找中点, reverse list, merge 2 lists) **************/
+    /**
+     * Time: O(3N) = O(N)  Space: O(1)
+     */
+    public void reorderList(ListNode head) {
+        // Step 1
+        // 找到中点
+        // 1 -> 2 -> 3 -> 4 is 2
+        // 1 -> 2 -> 3 -> 4 -> 5 is 3
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // Step 2
+        // reverse second half
+        //     null  null
+        //      |     |
+        // 1 -> 2    3 <- 4
+        //          null null
+        //           |    |
+        // 1 -> 2 -> 3   4 <- 5
+        ListNode prev = null, curr = slow.next;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        slow.next = null;
+        // Step 3
+        // merge 2 lists
+        ListNode tail = prev;
+        while (tail != null) {
+            ListNode tempHead = head.next;
+            ListNode tempTail = tail.next;
+            head.next = tail;
+            tail.next = tempHead;
+            head = tempHead;
+            tail = tempTail;
+        }
+    }
+
     private static void print(ListNode head) {
         List<Integer> res = new ArrayList<>();
         while (head != null) {
@@ -67,10 +113,10 @@ public class ReorderList {
 
         ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
         solution.reorderList(l1);
-        print(l1);
+        print(l1); // [1,5,2,4,3]
 
         ListNode l2 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
         solution.reorderList(l2);
-        print(l2);
+        print(l2); // [1,4,2,3]
     }
 }
