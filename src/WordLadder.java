05bd4c0fab6e103d718377edd6855600d656c528
Li Xie <xlabcba@gmail.com>
2024-10-25 21:29:55 -0700

    LC127

--- src/WordLadder.java
+++ src/WordLadder.java
@@ -4,54 +4,88 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * 127. Word Ladder (https://leetcode.com/problems/word-ladder/description/)
+ */
 public class WordLadder {
 
-    private static final String[] candidates =
-            new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    private static final char[] candidates = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
     /**************** Solution 1: Iterative BFS ************************/
     /**
+     * 最短路径，往往用 BFS，因为这样找到的一定是最短的！！！
+     *
      * Time: O(26KN) K denotes length of each word, N denotes size of wordSet.
+     * 每个 word 最多进 queue 一次，每个 word 处理是 26k 时间。
      * Space: O(N) queue & visited can be in same size as wordSet.
      */
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> wordSet = new HashSet<>();
-        for (String word : wordList) {
-            wordSet.add(word);
-        }
+    public int ladderLength1(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList); // or else TLE!!!
         if (!wordSet.contains(endWord) || beginWord.length() != endWord.length()) return 0;
-        int l = beginWord.length();
         Set<String> visited = new HashSet<>();
-        int step = 0;
-        List<String> queue = new ArrayList<>();
-        queue.add(beginWord);
-        visited.add(beginWord);
-        while (!queue.isEmpty()) {
-            List<String> nextQueue = new ArrayList<>();
-            for (String curr : queue) {
-                if (curr.equals(endWord)) return step + 1;
-                for (int i = 0; i < l; i++) {
-                    for (String c : candidates) {
-                        String next = curr.substring(0, i) + c + curr.substring(i+1);
-                        if (wordSet.contains(next) && !visited.contains(next)) {
-                            visited.add(next);
-                            nextQueue.add(next);
+        List<String> currLevel = new ArrayList<>();
+        currLevel.add(beginWord);
+        int length = 1;
+        while (!currLevel.isEmpty()) {
+            length++;
+            List<String> nextLevel = new ArrayList<>();
+            for (String word : currLevel) {
+                for (int i = 0; i < word.length(); i++) {
+                    for (char c : candidates) {
+                        String newWord = word.substring(0, i) + c + word.substring(i + 1);
+                        if (endWord.equals(newWord)) return length;
+                        if (!visited.contains(newWord) && wordSet.contains(newWord)) {
+                            visited.add(newWord);
+                            nextLevel.add(newWord);
                         }
                     }
                 }
             }
-            queue = nextQueue;
-            step++;
+            currLevel = nextLevel;
         }
         return 0;
     }
 
-    /********************** Solution 2: TODO: BFS 优化 两端向中间靠拢 ***************************/
-    // https://app.gitbook.com/o/-LEzdBqqw8FawHA3mleT/s/-LEzdBqr7mNEEfIbMstL/bfs-dfs/untitled#solution-4-iterative-bfs-jin-yi-bu-you-hua
+    /********************** Solution 2: BFS 优化 两端向中间靠拢 bi-directional BFS ***************************/
+    /**
+     * https://app.gitbook.com/o/-LEzdBqqw8FawHA3mleT/s/-LEzdBqr7mNEEfIbMstL/bfs-dfs/untitled#solution-4-iterative-bfs-jin-yi-bu-you-hua
+     * bi-directional BFS 的核心是 b^(d/2) + b^(d/2) < b^d
+     */
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList); // or else TLE!!!
+        if (!wordSet.contains(endWord)) return 0;
+        Set<String> beginSet = new HashSet<>(), endSet = new HashSet<>();
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        int length = 1;
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            length++;
+            if (beginSet.size() > endSet.size()) {
+                Set<String> temp = beginSet;
+                beginSet = endSet;
+                endSet = temp;
+            }
+            Set<String> nextSet = new HashSet<>();
+            for (String word : beginSet) {
+                for (int i = 0; i < word.length(); i++) {
+                    for (char c : candidates) {
+                        String newWord = word.substring(0, i) + c + word.substring(i + 1);
+                        if (endSet.contains(newWord)) return length;
+                        if (wordSet.remove(newWord)) {
+                            nextSet.add(newWord);
+                        }
+                    }
+                }
+            }
+            beginSet = nextSet;
+        }
+        return 0;
+    }
 
     public static void main(String[] args) {
         WordLadder solution = new WordLadder();
-        System.out.println(solution.ladderLength("hit","cog", Arrays.asList("hot","dot","dog","lot","log","cog")));
-        System.out.println(solution.ladderLength("hit","cog", Arrays.asList("hot","dot","dog","lot","log")));
+        System.out.println(solution.ladderLength("hit","cog", Arrays.asList("hot","dot","dog","lot","log","cog"))); // 5
+        System.out.println(solution.ladderLength("hit","cog", Arrays.asList("hot","dot","dog","lot","log"))); // 0
     }
 }
