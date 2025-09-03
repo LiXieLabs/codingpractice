276be60d3298a486079b90b31b1e1dd38dd51a8e
Li Xie <lxie1@paypal.com>
2025-09-03 14:32:34 -0700

    249

--- src/GroupShiftedStrings.java
+++ src/GroupShiftedStrings.java
@@ -8,7 +8,7 @@ import java.util.Map;
  */
 public class GroupShiftedStrings {
 
-    /************ Solution 1: Hashing Algo of Shifted Strings ******************/
+    /************ Solution 1: Hashing Algo of Shifted Strings => 最优解！！！ ******************/
     /**
      * 所有同一group的string必须经过某种hashing algo指向同一哈希值
      * hashing algo is:
@@ -29,7 +29,7 @@ public class GroupShiftedStrings {
     }
 
     private String getOriginalStr(String s) {
-        if (s.length() == 0 || s.charAt(0) == 'a') return s;
+        if (s.isEmpty() || s.charAt(0) == 'a') return s;
         int offset = s.charAt(0) - 'a';
         StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
@@ -38,9 +38,33 @@ public class GroupShiftedStrings {
         return sb.toString();
     }
 
+    /***************** Solution 2: 直接把差值做成 Hash Key ********************/
+    /**
+     * 容易想到，但是空间复杂度较差，需要分隔符，而且数字有可能两位，占位大！
+     */
+    public List<List<String>> groupStrings2(String[] strings) {
+        Map<String, List<String>> dict = new HashMap<>();
+        for (String s : strings) {
+            String key = buildKey(s);
+            dict.putIfAbsent(key, new ArrayList<>());
+            dict.get(key).add(s);
+        }
+        return new ArrayList<>(dict.values());
+    }
+
+    private String buildKey(String s) {
+        List<String> lst = new ArrayList<>();
+        for (int i = 1; i < s.length(); i++) {
+            int diff = s.charAt(i) - s.charAt(i - 1);
+            if (diff < 0) diff += 26; // za 的情况，diff == -25 < 0，+26 -> 1
+            lst.add(String.valueOf(diff));
+        }
+        return String.join(",", lst); // s.length() == 1 -> lst.size() == 0 -> ""
+    }
+
     public static void main(String[] args) {
         GroupShiftedStrings solution = new GroupShiftedStrings();
-        System.out.println(solution.groupStrings(new String[]{"abc","bcd","acef","xyz","az","ba","a","z"}));
-        System.out.println(solution.groupStrings(new String[]{"a"}));
+        System.out.println(solution.groupStrings(new String[]{"abc","bcd","acef","xyz","az","ba","a","z"})); // [[a, z], [abc, bcd, xyz], [az, ba], [acef]]
+        System.out.println(solution.groupStrings(new String[]{"a"})); // [[a]]
     }
 }
