352725cc09c312fde4c1ec93233b24374b6caa43
Li Xie <lxie1@paypal.com>
2025-12-16 21:42:03 -0800

    45

--- src/JumpGameII.java
+++ src/JumpGameII.java
@@ -18,7 +18,7 @@ public class JumpGameII {
      *
      * Time: O(N)   Space: O(1)
      */
-    public int jump(int[] nums) {
+    public int jump1(int[] nums) {
         int steps = 0, maxReach = 0, i = 0;
         while (maxReach < nums.length - 1) {
             int nexReach = maxReach;
@@ -27,6 +27,28 @@ public class JumpGameII {
             }
             if (nexReach == maxReach) return -1; // Optional if it is guaranteed to reach the end.
             maxReach = nexReach;
+            steps++;
+        }
+        return steps;
+    }
+
+    /*********** Solution 2: Solution 1 的 for loop 写法 **************/
+    /**
+     * 实际没必要！！！因为 i 实际是连贯的
+     * 但是容易想出来，可先写出来，再优化为 Solution 1
+     *
+     * Time: O(N)   Space: O(1)
+     */
+    public int jump(int[] nums) {
+        int start = 0, curReach = 0, steps = 0;
+        while (curReach < nums.length - 1) {
+            int nexReach = curReach;
+            for (int i = start; i <= curReach; i++) {
+                nexReach = Math.max(nexReach, i + nums[i]);
+            }
+            if (nexReach == curReach) return -1; // Optional if it is guaranteed to reach the end.
+            start = curReach + 1;
+            curReach = nexReach;
             steps++;
         }
         return steps;
