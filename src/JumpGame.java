2e55a4e3a9a7c5ef82fb72578234383be1db988e
Li Xie <lxie1@paypal.com>
2025-12-16 21:20:21 -0800

    55

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -7,13 +7,26 @@ public class JumpGame {
     /**
      * Time: O(N)   Space: O(1)
      */
-    public boolean canJump(int[] nums) {
+    public boolean canJump1(int[] nums) {
         int maxReach = 0;
         for (int i = 0; i < nums.length; i++) {
             if (maxReach < i) return false;
             maxReach = Math.max(maxReach, i + nums[i]);
         }
         return true;
+    }
+
+    /********** Solution 2: 另一个角度的 1D Greedy Algo ***************/
+    /**
+     * Time: O(N)   Space: O(1)
+     */
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i <= maxReach; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
